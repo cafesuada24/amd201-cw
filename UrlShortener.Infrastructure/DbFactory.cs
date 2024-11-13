d48ba63f2d0d24029f6110d40c3de39479ca78e4
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-11-13 18:49:29 +0700

    Implement interfaces

---
+++ UrlShortener.Infrastructure/DbFactory.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using UrlShortener.Infrastructure.Databases;
+
+namespace UrlShortener.Infrastructure;
+
+public class DbFactory : IDisposable
+{
+	private bool _disposed;
+	private Func<ShortUrlDbContext> _instanceFunc;
+	private DbContext _dbContext;
+	public DbContext DbContext => _dbContext ?? (_dbContext = _instanceFunc.Invoke());
+
+	public DbFactory(Func<ShortUrlDbContext> dbContextFactory)
+	{
+		_instanceFunc = dbContextFactory;
+	}
+
+	public void Dispose()
+	{
+		if (!_disposed && _dbContext != null)
+		{
+			_disposed = true;
+			_dbContext.Dispose();
+		}
+	}
+}
