c6c76a91c6c8abb12156c50bf01d3c531a908479
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-11-13 17:55:35 +0700

    Minor changes and add ShortenedUrl model

---
+++ UrlShortener.Domain/Entities/ShortenedUrl.cs
@@ -0,0 +1,12 @@
+namespace UrlShortener.Domain.Entities;
+
+public class ShortenedUrl : EntityBase<int>
+{
+    public Uri OriginalUrl { get; set; }
+    public Uri ShortUrl { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? ExpireAt { get; set; }
+    public int ClickCount { get; set; }
+    public DateTime? LastAccessTime { get; set; }
+    public UrlStatus Status { get; set; }
+}
