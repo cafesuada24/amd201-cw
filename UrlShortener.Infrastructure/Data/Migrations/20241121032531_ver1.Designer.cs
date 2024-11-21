7e52818306d8269efbbe36c13cfc0c6bb98e2164
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-11-21 10:41:38 +0700

    Update migrations

--- UrlShortener.Infrastructure/Data/Migrations/20241121032531_ver1.Designer.cs
+++ UrlShortener.Infrastructure/Data/Migrations/20241121032531_ver1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UrlShortener.Infrastructure.Databases;
 
@@ -11,9 +12,11 @@ using UrlShortener.Infrastructure.Databases;
 namespace UrlShortener.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(ShortUrlDbContext))]
-    partial class ShortUrlDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241121032531_ver1")]
+    partial class ver1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ UrlShortener.Infrastructure/Data/Migrations/20241121032531_ver1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrlShortener.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ver1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ShortenedUrls_ShortCode",
+                table: "ShortenedUrls",
+                column: "ShortCode",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ShortenedUrls_ShortCode",
+                table: "ShortenedUrls");
+        }
+    }
+}
