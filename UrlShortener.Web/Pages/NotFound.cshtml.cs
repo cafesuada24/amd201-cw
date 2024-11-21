4dbe445c9dc1ba823f8f770a73b6ce81dbc99d54
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-11-21 08:49:00 +0700

    Update Error page

---
+++ UrlShortener.Web/Pages/NotFound.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UrlShortener.Web.Pages;
+public class NotFoundModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
--- UrlShortener.Web/Program.cs
+++ UrlShortener.Web/Program.cs
@@ -14,6 +14,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts(); ;
 }
+app.UseStatusCodePagesWithReExecute("/NotFound");
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
